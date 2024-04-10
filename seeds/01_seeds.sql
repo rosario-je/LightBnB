0feae9b66d869b2067c7a7572d66b1a15d26bfa6
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-04-10 00:12:51 -0400

    Fix query

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -22,7 +22,7 @@ VALUES
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
 VALUES
-(3, 3, 1, 3, 'messages'),
+(3, 2, 1, 3, 'messages'),
 (2, 2, 2, 4, 'messages'),
 (3, 1, 3, 4, 'messages')
 ;
