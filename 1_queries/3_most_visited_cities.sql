7bc7fda78af10f84334a2d9ed6aedd20baf73713
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-04-17 12:34:33 -0400

    Refactor SQL queries for better readability and maintainability

--- 1_queries/3_most_visited_cities.sql
+++ 1_queries/3_most_visited_cities.sql
@@ -1,5 +1,10 @@
-SELECT city, COUNT(reservations) AS total_reservations
-FROM properties
-JOIN reservations ON properties.id = property_id
-GROUP BY city
-ORDER BY total_reservations DESC;
+SELECT
+  city,
+  COUNT(reservations) AS total_reservations
+FROM
+  properties
+  JOIN reservations ON properties.id = property_id
+GROUP BY
+  city
+ORDER BY
+  total_reservations DESC;
